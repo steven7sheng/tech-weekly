86fddc8d83966094de812ad2b27484a62ac73b80
steven7sheng <steven7sheng@163.com>
2021-06-28 10:05:06 +0000

    Deploying to gh-pages from  @ 7307ca963ed88b83b226d3bd23222f664246b528 🚀

--- page.js
+++ page.js
@@ -1,26 +1,39 @@
-fetch('./rss.json')
-  .then(async function (response) {
-    const res = await response.json();
-    const items = res.items;
 
-    const issues = document.querySelector('.issues');
-    const fragment = document.createDocumentFragment();
+  function fetchWeekly(url, selector) {
+    fetch(url)
+    .then(async function (response) {
+      const res = await response.json();
+      const items = res.items;
+  
+      const issues = document.querySelector(selector);
+      const fragment = document.createDocumentFragment();
+  
+      const title = document.createElement('summary');
+      title.innerText = res.title
+      title.classList.add('title', 'is-5', 'mb-2')
 
-    items.forEach(i => {
-      const issue = document.createElement('details');
-      const h2 = document.createElement('summary');
-      h2.classList.add('title', 'is-4');
-      const content = document.createElement('div');
+      fragment.appendChild(title)
 
-      const timeObj = new Date(i.pubDate);
-      const weekDays = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
-      h2.innerHTML = `${i.title} （<a href="${i.link}" target="_blank">${timeObj.toLocaleDateString()} ${weekDays[timeObj.getDay()]}</a>）`;
-      content.innerHTML = i.content;
+      items.forEach(i => {
+        const issue = document.createElement('details');
+        const h2 = document.createElement('summary');
+        const content = document.createElement('div');
+        content.classList.add('issue-content', 'notification',  'px-1', 'py-3')
+        const timeObj = new Date(i.pubDate);
+        const weekDays = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+        h2.innerHTML = `${i.title} （${timeObj.toLocaleDateString()} ${weekDays[timeObj.getDay()]} <a href="${i.link}" target="_blank" title=""></a>）`;
+        content.innerHTML = i.content;
+  
+        issue.appendChild(h2);
+        issue.appendChild(content);
+        fragment.appendChild(issue);
+      });
+  
+      issues.appendChild(fragment);
+    })
+  
+  }
 
-      issue.appendChild(h2);
-      issue.appendChild(content);
-      fragment.appendChild(issue);
-    });
 
-    issues.appendChild(fragment);
-  })
+fetchWeekly('./js.json', '.issues-js')
+fetchWeekly('./fe.json', '.issues-fe')
